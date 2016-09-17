336b1cbe33f6a30ec69186844685e51af70237f5
Maxim Ryklis <maxim.ryklis@spsmail.cuny.edu>
2016-09-17 18:48:30 +0000

    completed task_03. 3 functions. with the last calling the two above

--- task_01.py
+++ task_01.py
@@ -35,12 +35,33 @@ def celsius_to_kelvin(degrees):
       ABSOLUTE_DIFFERENCE (dec): value of 273.15
 
     Returns:
-      kelvin (int): 'degrees' + ABSOLUTE_DIFFERENCE
+      kelvin (dec): 'degrees' + ABSOLUTE_DIFFERENCE
 
     Examples:
-      >>>fahrenheit_to_celsius(212)
-      Decimal('100')
+      >>>celsius_to_kelvin(100)
+      Decimal('373.15')
     """
     kelvin = (degrees + ABSOLUTE_DIFFERENCE)
 
     return kelvin
+
+
+def fahrenheit_to_kelvin(degrees):
+    """calculate fahrenheit as kelvin. decimal format.
+
+    Args:
+      degrees (int): temp value
+      ABSOLUTE_DIFFERENCE (dec): value of 273.15
+      fahrenheit_to_celsius (func): converts to celsius
+      celsius_to_fahrenheit (func): converts to kelvin
+
+    Returns:
+      kelvinf (dec): converted to celsius, then to kelvin
+
+    Examples:
+      >>>fahrenheit_to_kelvin(212)
+      Decimal('373.15')
+    """
+    kelvinf = celsius_to_kelvin(fahrenheit_to_celsius(degrees))
+
+    return kelvinf
